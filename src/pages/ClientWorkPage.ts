706c5fd315ed23fb4d619b65e558de801ebffe4d
Aadi Jain <aadij4945@gmail.com>
2025-09-08 18:48:46 +0530

    Create src/pages/ClientWorkPage.ts

---
+++ src/pages/ClientWorkPage.ts
@@ -0,0 +1,13 @@
+import { Page } from '@playwright/test';
+
+export class ClientWorkPage {
+  constructor(private page: Page) {}
+
+  async isClientWorkHeadingVisible() {
+    return this.page.getByRole('heading', { name: /Client Work/i }).isVisible();
+  }
+
+  async getClientWorkHeadingText() {
+    return this.page.getByRole('heading', { name: /Client Work/i }).innerText();
+  }
+}
