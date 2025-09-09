cc2a11024e6e464bbae9d9a5d53608f093c8a318
Aadi Jain <aadij4945@gmail.com>
2025-09-09 14:05:49 +0530

    Create src/pages/HeaderPage.ts

---
+++ src/pages/HeaderPage.ts
@@ -0,0 +1,20 @@
+import { Page } from '@playwright/test';
+
+export class HeaderPage {
+  constructor(private page: Page) {}
+
+  async selectServices() {
+    await this.page.getByRole('link', { name: 'Services' }).hover();
+  }
+
+  async clickExploreOurClientWork() {
+    await this.page.getByRole('link', { name: 'Explore Our Client Work' }).click();
+  }
+
+  async dismissCookieConsent() {
+    const consentButton = this.page.getByRole('button', { name: 'Accept All' });
+    if (await consentButton.isVisible()) {
+      await consentButton.click();
+    }
+  }
+}
